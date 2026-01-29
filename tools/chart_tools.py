dcd8814640f381be6cb389b00b22cc5ca6f4bf5e
Suyash Bhatkar <94097662+SKB3002@users.noreply.github.com>
2026-01-29 12:03:34 +0530

    Add function to generate charts with matplotlib

--- tools/chart_tools.py
+++ tools/chart_tools.py
@@ -1,1 +1,26 @@
+import matplotlib.pyplot as plt
+import uuid
+
+
+def generate_chart(data: dict, chart_type: str, title: str):
+    chart_id = str(uuid.uuid4())
+    file_path = f"charts/{chart_id}.png"
+
+    keys = list(data.keys())
+    values = list(data.values())
+
+    plt.figure()
+    if chart_type == "bar":
+        plt.bar(keys, values)
+    elif chart_type == "line":
+        plt.plot(keys, values)
+    else:
+        raise ValueError("Unsupported chart type")
+
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(file_path)
+    plt.close()
+
+    return file_path
 
