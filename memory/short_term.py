b082fbd9dffee831ee2660e435fce757d5e1301e
Suyash Bhatkar <94097662+SKB3002@users.noreply.github.com>
2026-01-29 11:55:47 +0530

    Implement ShortTermMemory class with basic methods

--- memory/short_term.py
+++ memory/short_term.py
@@ -1,1 +1,14 @@
+class ShortTermMemory:
+    def __init__(self, run_id):
+        self.run_id = run_id
+        self.store = {}
+
+    def add(self, key, value):
+        self.store[key] = value
+
+    def get(self, key):
+        return self.store.get(key)
+
+    def dump(self):
+        return self.store
 
